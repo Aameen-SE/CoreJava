d848f576f6a6b92d60dc0659ac9ee5ff1166dc55
Aameen-SE <aameen.se@gmail.com>
2024-07-01 12:02:39 +0530

    all even number from 1 to 100

---
+++ Student/src/org/jn/aameen/array/flowcontrol/AllEvenNumber.java
@@ -0,0 +1,17 @@
+package org.jn.aameen.array.flowcontrol;
+
+public class AllEvenNumber {
+
+	public static void main(String[] args) {
+		// Print all even numbers from 1 to 100
+		
+		for(int i =2;i<=100;i++)
+		{
+			if(i%2==0)
+				System.out.print(i+",");
+		}
+		
+
+	}
+
+}
