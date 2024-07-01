d9fde8dfaebea6a9f0f47d71517014d92a656a0d
Aameen-SE <aameen.se@gmail.com>
2024-07-01 12:40:29 +0530

    Sum of all number from 1 to 100 usind for loop

---
+++ Student/src/org/jn/aameen/array/flowcontrol/PrintSumofAllNumber.java
@@ -0,0 +1,17 @@
+package org.jn.aameen.array.flowcontrol;
+
+public class PrintSumofAllNumber {
+
+	public static void main(String[] args) {
+		// Print the sum of all numbers from 1 to 100.
+		int num = 100;
+		int sum =0;
+		for(int i =1;i<=100;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println("Sum of all the number from 1 to 100 is , sum :"+sum);
+
+	}
+
+}
