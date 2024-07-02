4bacd337e21ac43d9600f5dbf2d100b1e3aa1224
Aameen-SE <aameen.se@gmail.com>
2024-07-02 12:15:07 +0530

    changes

--- Student/src/org/jn/aameen/method/PrimeMethod.java
+++ Student/src/org/jn/aameen/method/PrimeMethod.java
@@ -10,32 +10,10 @@ public class PrimeMethod {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number ");
 		int num = sc.nextInt();
-		//Prime(num);
-		//boolean ans =isPrime(num);
-		//System.out.println("isPrime : "+ans);
-		PrimeorNot(num);
+		Prime(num);
+		boolean ans =isPrime(num);
+		System.out.println("isPrime : "+ans);
 	}
-	
-	static void PrimeorNot(int n) {
-		for(int i=2;i>=n;i++) {
-			if(n%i!=0)
-				System.out.println(i+";");
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	static void Prime(int n) {
 		if(n<=2)
 		{
