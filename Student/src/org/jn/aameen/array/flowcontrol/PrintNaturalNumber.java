71e817a2c37ba898cb08281afe0ccb66a47d9559
Aameen-SE <aameen.se@gmail.com>
2024-07-03 18:27:11 +0530

    first 10 natural number

---
+++ Student/src/org/jn/aameen/array/flowcontrol/PrintNaturalNumber.java
@@ -0,0 +1,15 @@
+package org.jn.aameen.array.flowcontrol;
+
+public class PrintNaturalNumber {
+
+	public static void main(String[] args) {
+		// Print the first 10 natural numbers
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
