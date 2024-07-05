3c6c4ff8cf27e280d98d5f73a0d9eda11cb576b7
Aameen-SE <aameen.se@gmail.com>
2024-07-05 16:25:21 +0530

    swapping of array value in array

---
+++ Student/src/org/jn/aameen/array/SwapArray.java
@@ -0,0 +1,19 @@
+package org.jn.aameen.array;
+import java.util.Arrays;
+
+public class SwapArray {
+
+	public static void main(String[] args) {
+		// swap 
+		int [] arr = {1 , 2 , 3 , 4 , 5};
+		
+		swap(arr , 1,3);
+		System.out.println(Arrays.toString(arr));
+	}
+	static void swap(int[] arr , int index1 , int index2) {
+		int temp = arr[index1];
+		arr[index1]=arr[index2];
+		arr[index2]=temp;
+	}
+
+}
