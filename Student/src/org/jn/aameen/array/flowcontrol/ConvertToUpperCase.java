31369e5718440a763b213fe8a961862535b1840d
Aameen-SE <aameen.se@gmail.com>
2024-07-06 22:37:19 +0530

    character to uppercase

---
+++ Student/src/org/jn/aameen/array/flowcontrol/ConvertToUpperCase.java
@@ -0,0 +1,16 @@
+package org.jn.aameen.array.flowcontrol;
+
+
+public class ConvertToUpperCase {
+
+	public static void main(String[] args) {
+		// Convert a character to uppercase.
+		
+		char ch ='m';
+		char ch1 = Character.toUpperCase(ch);
+		
+		System.out.println(ch1);
+
+	}
+
+}
