a6e594016ef5ad7f16c0ac3a7acb1414bfdf732c
Aameen-SE <aameen.se@gmail.com>
2024-07-19 19:19:30 +0530

    position of string using charAt

---
+++ Student/src/org/jn/aameen/string/StringPosition.java
@@ -0,0 +1,14 @@
+package org.jn.aameen.string;
+
+public class StringPosition {
+
+	public static void main(String[] args) {
+		// position of string 
+		
+		String name="Aameem";
+		
+		System.out.println(name.charAt(0));
+
+	}
+
+}
