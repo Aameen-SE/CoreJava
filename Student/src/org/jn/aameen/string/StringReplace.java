fb4eca1edcd503cb16366508ffc69c99a140d0e6
Aameen-SE <aameen.se@gmail.com>
2024-07-19 19:23:56 +0530

    using replace in string

---
+++ Student/src/org/jn/aameen/string/StringReplace.java
@@ -0,0 +1,14 @@
+package org.jn.aameen.string;
+
+public class StringReplace {
+
+	public static void main(String[] args) {
+		// using replace 
+		String name = "Abmeen";
+		String name2 =name.replace("b", "a");
+		
+		System.out.println(name2);
+
+	}
+
+}
