8a636cf3b6b47748a5e97226e7d02fb34f089d71
Aameen-SE <aameen.se@gmail.com>
2024-07-19 19:29:16 +0530

    using of substring to take short string character from a long character

---
+++ Student/src/org/jn/aameen/string/SubString.java
@@ -0,0 +1,13 @@
+package org.jn.aameen.string;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		// using substring
+		
+		String name = "Aameen Hussain";
+		System.out.println(name.substring(0, 7));
+
+	}
+
+}
