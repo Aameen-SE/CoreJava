685a72bea9218994a3ae248d9fb9aaadb1953b9b
Aameen-SE <aameen.se@gmail.com>
2024-07-19 19:20:58 +0530

    length of the string

---
+++ Student/src/org/jn/aameen/string/StringLength.java
@@ -0,0 +1,13 @@
+package org.jn.aameen.string;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		// length of a string 
+		
+		String name="Aameen";
+		System.out.println(name.length());
+
+	}
+
+}
