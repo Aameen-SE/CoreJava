a3a848b98da2f247ba811173b4e4460036fd648f
Aameen-SE <aameen.se@gmail.com>
2024-07-19 20:01:27 +0530

    maths random number into int datatype (explicit casting )

---
+++ Student/src/org/jn/aameen/mathclass/RandomtoInt.java
@@ -0,0 +1,12 @@
+package org.jn.aameen.mathclass;
+
+public class RandomtoInt {
+
+	public static void main(String[] args) {
+		// maths random number into int datatype 
+		
+		System.out.println((int)(Math.random()*100));
+
+	}
+
+}
