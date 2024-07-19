c1d4355167296c158f358940aa7a6303934a8591
Aameen-SE <aameen.se@gmail.com>
2024-07-19 19:12:05 +0530

    adding two string

---
+++ Student/src/org/jn/aameen/string/StringProgram.java
@@ -0,0 +1,15 @@
+package org.jn.aameen.string;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		// adding two string
+		
+		String name1="Aameen";
+		String name2="Hussain";
+		String name3=name1+name2;
+		System.out.println(name3);
+
+	}
+
+}
