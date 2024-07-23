97361c5a601b2712fe6efe115a9799d2fa93e65d
Aameen-SE <aameen.se@gmail.com>
2024-07-23 18:07:37 +0530

    Star pattern 2

---
+++ Student/src/com/aameen/logicbuilding/StarPattern2.java
@@ -0,0 +1,32 @@
+package com.aameen.logicbuilding;
+
+public class StarPattern2 {
+
+	public static void main(String[] args) {
+						/*Star Pattern 2
+				
+						* * * * * 
+				
+						* * * * 
+				
+						* * * 
+				
+						* * 
+				
+						*   */
+						
+		
+		for(int i=5;i>=1;i--)
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+			
+		}
+		
+
+	}
+
+}
