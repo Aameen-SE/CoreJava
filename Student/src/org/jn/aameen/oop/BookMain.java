231c8b2a1c53d09d3ec57daf40b8b3c8f3d32aa6
Aameen-SE <aameen.se@gmail.com>
2024-07-23 17:43:09 +0530

    book class calling the setter and getter from the other

---
+++ Student/src/org/jn/aameen/oop/BookMain.java
@@ -0,0 +1,36 @@
+package org.jn.aameen.oop;
+
+public class BookMain {
+
+	public static void main(String[] args) {
+		
+		// creating an object 
+		
+		Book book = new Book();
+		
+		
+		book.setAuthor("Antonio");
+		System.out.println("Authorname :"+book.getAuthor());
+		
+		
+		
+		// calling members method 
+	
+		book.setvalue(32.5, "Charles ", 100 , 32);
+		book.display();
+		
+		int age=book.getAge();
+		
+		if(age<18)
+			System.out.println("Kid");
+		else
+			System.out.println("Adult");
+	
+		
+		
+		
+		
+		
+	}
+
+}
