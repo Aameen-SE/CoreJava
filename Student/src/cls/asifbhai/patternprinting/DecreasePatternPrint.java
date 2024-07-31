f608ecd2a7c13a83316a41e01dd94441f3f78215
Aameen-SE <aameen.se@gmail.com>
2024-07-31 21:27:33 +0530

    Decrease Pattern Printing

---
+++ Student/src/cls/asifbhai/patternprinting/DecreasePatternPrint.java
@@ -0,0 +1,22 @@
+package cls.asifbhai.patternprinting;
+
+public class DecreasePatternPrint {
+
+	public static void main(String[] args) {
+		// Pattern Print
+		
+		int num=5;
+		
+		for(int i=1;i<=num;i++)
+		{
+			for(int j=i;j<=num;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+			
+		}
+
+	}
+
+}
